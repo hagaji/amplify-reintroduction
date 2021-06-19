b4366e89b6a43eebdad6e2f02eef781c45f3fdff
Takeshi Haga <13972608+hagaji@users.noreply.github.com>
2021-06-19 22:57:40 +0900

    update app

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React DEATH
+          Learn React DEATH and REBIRTH
         </a>
       </header>
     </div>
